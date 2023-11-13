2f7cedf531f2fa5b94833cb4ba6c048681789e71
codelyfe <codelyfe@gmail.com>
2023-11-12 22:13:43 -0600

    Update english.php

--- text-content/english.php
+++ text-content/english.php
@@ -1,12 +1,12 @@
 <?php
 // ADMIN
-$admintitle = "(ADMIN) Stupid Simple CMS";
+$admintitle = "<i class='fa-solid fa-blog'></i> (ADMIN) Stupid Simple CMS";
 $loginpagegreeting = "Join the virtual story today!";
 $submitanarticle = "Submit an Article <i class='fa-solid fa-feather'></i>";
 $logoutbutton = "Logout";
 $blogbutton = "Blog";
 // USER
-$sitetitle = "Stupid Simple CMS";
+$sitetitle = "<i class='fa-solid fa-blog'></i> Stupid Simple CMS";
 $category = "Category: ";
 $createdat = "Created at: <i class='fa-regular fa-clock'></i> ";
 $searchbutton = "<i class='fa-solid fa-magnifying-glass'></i> Search";
