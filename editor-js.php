5a41b14a5191c67d2bb9e30385c22fd42964b2f4
codelyfe <codelyfe@gmail.com>
2023-11-18 11:18:00 -0600

    Update

--- editor-js.php
+++ editor-js.php
@@ -17,7 +17,7 @@
 <body>
 <?php require_once 'layout/navbar-admin.php'; ?>
 <br/><br/>
-<h1 class="mx-auto" style="text-align: center; color:white;">Custom JS<br /><a href="add-article.php" class="btn btn-dark"><i class="fa-solid fa-feather" style="color: #ffc107;"></i> Add Article</a> <a href="design-blog.php" class="btn btn-dark"><i class="fa-solid fa-brush" style="color: #ffc107;"></i> Design Blog</a> <a href="admin-edit.php" class="btn btn-dark"><i class="fa-solid fa-pen-to-square" style="color: #ffc107;"></i> Edit Articles</a> <a href="index.php" class="btn btn-dark"><i class="fa-regular fa-rectangle-list" style="color: #ffc107;"></i> Blog</a></h1>
+<h1 class="mx-auto" style="text-align: center; color:white;">Custom <i class="fa-brands fa-js" style="color: #ffc107;"></i><br /><a href="add-article.php" class="btn btn-dark"><i class="fa-solid fa-feather" style="color: #ffc107;"></i> Add Article</a> <a href="design-blog.php" class="btn btn-dark"><i class="fa-solid fa-brush" style="color: #ffc107;"></i> Design Blog</a> <a href="admin-edit.php" class="btn btn-dark"><i class="fa-solid fa-pen-to-square" style="color: #ffc107;"></i> Edit Articles</a> <a href="index.php" class="btn btn-dark"><i class="fa-regular fa-rectangle-list" style="color: #ffc107;"></i> Blog</a></h1>
 <br/>
 <div class="container" style="background:white;padding:50px;border-radius:10px;">
 
@@ -42,7 +42,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['js_content'])) {
 <form method="post" action="">
     <textarea class="form-control" name="js_content" rows="10" cols="50"><?php echo $jsContent; ?></textarea>
     <br>
-    <input class="form-control" type="submit" value="Save JavaScript">
+    <input class="form-control" type="submit" value="Save  JavaScript">
 </form>
 
 </div>
