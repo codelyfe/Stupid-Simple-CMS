1499496674557fd9cae4482fde15e0304348459c
codelyfe <codelyfe@gmail.com>
2023-11-18 17:55:36 -0600

    Update fetch_files.php

--- file-manager/fetch_files.php
+++ file-manager/fetch_files.php
@@ -10,7 +10,7 @@ if ($files) {
             $filename = basename($file);
             $fileUrl = 'https://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/' . $uploadFolder . $filename;
 
-            $html .= '<li styl="float:right;">' . $filename . 
+            $html .= '<li>' . $filename . 
                      ' <button class="btn btn-danger" onclick="deleteFile(\'' . $filename . '\')"><i class="fa-solid fa-trash"></i></button>' .
                      ' <button class="btn btn-dark" onclick="copyFileUrl(\'' . $fileUrl . '\')"><i class="fa-regular fa-clipboard"></i></button>' .
                      ' <button class="btn btn-dark" onclick="renameFile(\'' . $filename . '\', prompt(\'Enter new name:\', \'' . $filename . '\'))">Rename</button>' .
