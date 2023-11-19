8e303ba34978a3b8d574993842cfb087465b90ad
codelyfe <codelyfe@gmail.com>
2023-11-18 18:04:42 -0600

    Update fetch_files.php

--- file-manager/fetch_files.php
+++ file-manager/fetch_files.php
@@ -10,13 +10,30 @@ if ($files) {
             $filename = basename($file);
             $fileUrl = 'https://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/' . $uploadFolder . $filename;
 
-            $html .= '<li>' . $filename . 
-                    ' <div style="float: right;">' .
-                    '     <button class="btn btn-danger" onclick="deleteFile(\'' . $filename . '\')"><i class="fa-solid fa-trash"></i></button>' .
-                    '     <button class="btn btn-dark" onclick="copyFileUrl(\'' . $fileUrl . '\')"><i class="fa-regular fa-clipboard"></i></button>' .
-                    '     <button class="btn btn-dark" onclick="renameFile(\'' . $filename . '\', prompt(\'Enter new name:\', \'' . $filename . '\'))">Rename</button>' .
-                    ' </div>' .
-                    '</li>';
+            // Check if the file is an image by checking its extension
+            $imageExtensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp'];
+            $fileExtension = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
+
+            if (in_array($fileExtension, $imageExtensions)) {
+                // If it's an image file, display thumbnail
+                $html .= '<li>' . 
+                         ' <img src="' . $fileUrl . '" alt="' . $filename . '" style="max-width: 100px; max-height: 100px;"> ' . $filename .
+                         ' <div style="float: right;">' .
+                         '     <button class="btn btn-danger" onclick="deleteFile(\'' . $filename . '\')"><i class="fa-solid fa-trash"></i></button>' .
+                         '     <button class="btn btn-dark" onclick="copyFileUrl(\'' . $fileUrl . '\')"><i class="fa-regular fa-clipboard"></i></button>' .
+                         '     <button class="btn btn-dark" onclick="renameFile(\'' . $filename . '\', prompt(\'Enter new name:\', \'' . $filename . '\'))">Rename</button>' .
+                         ' </div>' .
+                         '</li>';
+            } else {
+                // For non-image files, display the filename without thumbnail
+                $html .= '<li>' . $filename . 
+                         ' <div style="float: right;">' .
+                         '     <button class="btn btn-danger" onclick="deleteFile(\'' . $filename . '\')"><i class="fa-solid fa-trash"></i></button>' .
+                         '     <button class="btn btn-dark" onclick="copyFileUrl(\'' . $fileUrl . '\')"><i class="fa-regular fa-clipboard"></i></button>' .
+                         '     <button class="btn btn-dark" onclick="renameFile(\'' . $filename . '\', prompt(\'Enter new name:\', \'' . $filename . '\'))">Rename</button>' .
+                         ' </div>' .
+                         '</li>';
+            }
         }
     }
 }
