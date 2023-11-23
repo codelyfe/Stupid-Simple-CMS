76316ecdff64fe2fd499679f31d1cfcd246bebbf
codelyfe <codelyfe@gmail.com>
2023-11-22 21:49:56 -0600

    Update admin-edit.php

--- admin-edit.php
+++ admin-edit.php
@@ -42,7 +42,7 @@
     <body class="text-center">
         <?php require_once 'layout/navbar-admin.php'; ?>
         <br /><br />
-        <h1 class="mb-4">Article Editor <br /> 
+        <h1 class="mb-4"><i class="fa-solid fa-pen-to-square" style="color: #ffc107;"></i> Article Editor <br /> 
         <a href="<?php echo $websiteUrl; ?>add-article.php" class="btn btn-dark"><i class="fa-solid fa-feather" style="color: #ffc107;"></i> Submit an Article</a> 
         <a href="<?php echo $websiteUrl; ?>index.php" class="btn btn-dark"><i class='fa-regular fa-rectangle-list' style="color: #ffc107;"></i> <?php echo $blogbutton; ?></a></h1>
 
