d9d5ba28aa90407ed4e1a422e1216f667bec5efb
codelyfe <codelyfe@gmail.com>
2023-11-22 23:11:16 -0600

    Update index.php

--- help/index.php
+++ help/index.php
@@ -16,7 +16,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>File Manager</title>
+    <title>Help Desk</title>
     <link rel="stylesheet" type="text/css" href="../css/custom.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
